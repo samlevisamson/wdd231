8691e4d2fa724079eb112133826fc9cfd6694ead
Sam Levi Samson <sam.levi.samson@gmail.com>
2026-03-14 16:48:57 +0530

    week2 learning activites

---
+++ scripts/prophets.js
@@ -0,0 +1,46 @@
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+      // Create elements to add to the div.cards element
+      let card = document.createElement('section');
+      let fullName = document.createElement('h2');
+      let birthDay = document.createElement('p');
+      let birthPlace = document.createElement('p');
+      let portrait = document.createElement('img');
+  
+      // Build the h2 content out to show the prophet's full name
+      fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+
+      // Build the h2 content out to show the prophet's full name
+      birthDay.textContent = `Date of Birth: ${prophet.birthdate}`;
+
+      // Build the h2 content out to show the prophet's full name
+      birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+  
+      // Build the image portrait by setting all the relevant attributes
+      portrait.setAttribute('src', prophet.imageurl);
+      portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+      portrait.setAttribute('loading', 'lazy');
+      portrait.setAttribute('width', '340');
+      portrait.setAttribute('height', '440');
+  
+      // Append the section(card) with the created elements
+      card.appendChild(fullName);
+      card.appendChild(birthDay);
+      card.appendChild(birthPlace);
+      card.appendChild(portrait);
+      
+    
+      cards.appendChild(card);
+    });
+}
+async function getProphetData() {
+    const response = await fetch(url); // request
+    const data = await response.json(); // parse the JSON data
+    //console.table(data.prophets); // temp output test of data response 
+    displayProphets(data.prophets); // note that you reference the prophets array of the JSON data object, not just the object
+  }
+  
+  getProphetData();
